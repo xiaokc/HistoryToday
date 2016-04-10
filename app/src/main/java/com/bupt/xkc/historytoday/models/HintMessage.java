4181fc72d554585c12b74166ceec910d1f57eae9
xkc <xkc@test.com>
2016-04-10 11:09:40 +0800

    一些常量

---
+++ app/src/main/java/com/bupt/xkc/historytoday/models/HintMessage.java
@@ -0,0 +1,14 @@
+package com.bupt.xkc.historytoday.models;
+
+/**
+ * Created by xkc on 4/8/16.
+ */
+public class HintMessage {
+    public static final String NO_DATA = "no data return";
+    public static final String NO_DATA_SHOW = "啊哦，历史上的今天没发生什么大事啊o(╯□╰)o";
+    public static final String NO_EVENT_DETAIL = "抱歉，该事件缺乏详细记载o(╯□╰)o";
+    public static final String IMAGE_DOWNLOAD_ERROR = "图片下载失败";
+    public static final String IMAGE_EXIST = "图片已存在";
+    public static final String IMAGE_DOWNLOAD_FINISH = "图片下载完成";
+
+}
